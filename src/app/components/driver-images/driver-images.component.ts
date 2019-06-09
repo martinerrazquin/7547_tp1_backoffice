742908b3c8ec3ac3d31645dddb0e2d8495c2ef39
Martin Errazquin <martin.errazquin@hotmail.com>
2019-06-09 05:00:52 -0300

    now fetching real pictures for driver, still need to show them

--- src/app/components/driver-images/driver-images.component.ts
+++ src/app/components/driver-images/driver-images.component.ts
@@ -1,6 +1,9 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material';
 
+import { DriversService } from "../../services/drivers.service";
+import {FullDriver} from "../../models/full-driver";
+
 @Component({
   selector: 'app-driver-images',
   templateUrl: './driver-images.component.html',
@@ -10,11 +13,21 @@ export class DriverImagesComponent implements OnInit {
 
   driverId: number;
   showingWhich: number;
+  driverData: FullDriver;
+  isLoading: boolean = true;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+              private driversService: DriversService) { }
 
   ngOnInit() {
     this.driverId = this.data.id;
+    this.driversService.getDriverImages(this.driverId).subscribe(
+      (fullDriverData) => {
+        this.driverData = fullDriverData;
+        this.isLoading = false;
+        console.log(this.driverData); //DEBUG
+      }
+    )
   }
 
   onShowPicClick(which: number): void {
